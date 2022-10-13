4cac637878d06e2abbe69d8d84006f776926ba04
Breadosaurus <jobwong@ucsc.edu>
2022-10-12 23:29:42 -0700

    Lab 4

---
+++ lab4/js/lab.js
@@ -0,0 +1,16 @@
+// Checkers Game Pseudocode.
+// This is a commented version of our list creating an automated checkers game.
+// @author Joann Marie Wong, Cyrus Martinez-Weaver.
+// @since  10/12/2022
+
+
+
+//Task 3:
+//Creating a Checkers Game (Against a Computer)
+  //Create the game board before each game
+    //Create an opponent
+    //Establish the player’s color and side of the board
+  //Validate user’s possible moves
+    //Evaluate possible moves
+    //Choose a computer move
+  //Create an end/win condition 
