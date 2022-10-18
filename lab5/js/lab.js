efe74f4b372cd7545e9f45fa682cdb9c283588c0
Breadosaurus <jobwong@ucsc.edu>
2022-10-17 17:29:25 -0700

    Lab 5

---
+++ lab5/js/lab.js
@@ -0,0 +1,24 @@
+/*
+Author: Wong, Joann <jobwong@ucsc.edu> and Martinez-Weaver, Cyrus <cycmarti@ucsc.edu>
+Created: 17 October
+License: Public Domain
+
+*/
+
+// Define Variables
+var make = "Ford"
+var model = "Pinto"
+var color = "Blurple"
+var year =  "2001"
+
+ownIt = true
+// calculate
+var age = 2022 - year
+
+//output
+
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
